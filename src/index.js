760e8e3f9d339455efc80581bcdc7822054def0e
Caio Capua <caiocapua@hotmail.com>
2020-10-24 14:18:30 -0300

    Consumindo API com axios, usando FlatList, função para adição de um novo projeto e useState e useEffect, respeito o conceito de imutabilidade

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList,Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            console.log(response.data);
+            setProjects(response.data);
+        });
+    }, []);
+
+   async function handleAddProject() {
+    const response = await api.post('projects', {
+        title: `Novo Projeto ${Date.now()}`,
+        owner: 'Caio Capua'
+    });
+
+        const project = response.data;
+
+        setProjects([ ...projects, project ]);
+    }
+
     return(
         <>
             <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-            <View style={styles.container}>
-                <Text style={styles.text}>Hello World</Text>
-            </View>
+
+            <SafeAreaView style={styles.container}>
+                <FlatList 
+                    data={projects} 
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
+                        <Text style={styles.text}>{project.title}</Text>
+                    )}
+                />
+
+                <TouchableOpacity 
+                    style={styles.button}  
+                    activeOpacity={0.6} 
+                    onPress={handleAddProject}
+                >
+                    <Text style={styles.buttonText}>Adicionar Projeto</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
         </>
     )
 }
@@ -16,12 +53,21 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#7159c1',
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
     text: {
         color: '#fff',
-        fontSize: 32,
-        fontWeight: "bold",
+        fontSize: 30,
+    },
+    button: {
+        backgroundColor: '#fff',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
     }
 });
